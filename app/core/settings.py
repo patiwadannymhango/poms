9e499646a85fd57743986b9c347d4c28e82ae974
patiwadannymhango <52204968+patiwadannymhango@users.noreply.github.com>
2026-05-14 13:19:13 +0200

    Changed The Title

--- app/core/settings.py
+++ app/core/settings.py
@@ -46,8 +46,8 @@ INSTALLED_APPS = [
 
 JAZZMIN_SETTINGS = {
     "site_title": "TKEL PO System",
-    "site_header": "TKEL POMS",
-    "site_brand": "TKEL POMS",
+    "site_header": "TKEL POMX",
+    "site_brand": "TKEL POMX",
     "welcome_sign": "Purchase Order Management System", # Modernized text
     "copyright": "Tripple K Engineering Limited",
 
